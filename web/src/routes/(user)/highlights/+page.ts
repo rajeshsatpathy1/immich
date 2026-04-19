2dcdf619dd4afb3f9012f5029a1ebff43258c651
Rajesh Satpathy <rajesh.a.s.1997@gmail.com>
2026-04-19 18:31:33 -0500

    feat: add feature flag check for highlights in user routes

--- web/src/routes/(user)/highlights/+page.ts
+++ web/src/routes/(user)/highlights/+page.ts
@@ -1,10 +1,15 @@
 import { authenticate } from '$lib/utils/auth';
+import { featureFlagsManager } from '$lib/managers/feature-flags-manager.svelte';
 import { getFormatter } from '$lib/utils/i18n';
 import { searchHighlights } from '@immich/sdk';
+import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ url }) => {
   const user = await authenticate(url);
+  if (!featureFlagsManager.value.highlights) {
+    redirect(307, '/photos');
+  }
   const $t = await getFormatter();
   const highlights = await searchHighlights({});
 
--- web/src/routes/(user)/highlights/[id]/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/highlights/[id]/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -1,10 +1,15 @@
 import { authenticate } from '$lib/utils/auth';
+import { featureFlagsManager } from '$lib/managers/feature-flags-manager.svelte';
 import { getFormatter } from '$lib/utils/i18n';
 import { getHighlight } from '@immich/sdk';
+import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params, url }) => {
   const user = await authenticate(url);
+  if (!featureFlagsManager.value.highlights) {
+    redirect(307, '/photos');
+  }
   const $t = await getFormatter();
   const highlight = await getHighlight({ id: params.id });
 
